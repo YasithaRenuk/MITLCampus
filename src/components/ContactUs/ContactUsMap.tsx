4d52066c43a947d4ade0e7149da6efa4c48c269a
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2026-01-21 20:57:37 +0530

    map

--- src/components/ContactUs/ContactUsMap.tsx
+++ src/components/ContactUs/ContactUsMap.tsx
@@ -1,15 +1,13 @@
 // ContactUsMap.tsx
-import Image from "next/image";
-
 export default function ContactUsMap() {
   return (
-    <div className="relative w-full h-120 -top-10">
-      <Image
-        src="/ContactUs/LocationImage.png"
-        alt="Map"
-        fill
-        className="object-cover"
-        priority
+    <div className="relative w-full h-120 -top-10 overflow-hidden rounded-lg">
+      <iframe
+        title="Google Map"
+        src="https://www.google.com/maps?q=RVWW+MMM,+Sri+Pagngnasara+Mawatha,+Galle+80000&output=embed"
+        className="absolute inset-0 w-full h-full border-0"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
       />
     </div>
   );
